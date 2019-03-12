3c75798287079e56c63e5e650b8d5e6583c81eff
Celicath <celicath@gmail.com>
2019-03-13 00:25:01 +0900

    Add new card texts

--- src/main/java/ThePokerPlayer/cards/FakeSymbols.java
+++ src/main/java/ThePokerPlayer/cards/FakeSymbols.java
@@ -24,7 +24,7 @@ public class FakeSymbols extends CustomCard {
 	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 	private static final AbstractCard.CardType TYPE = CardType.POWER;
 	private static final AbstractCard.CardColor COLOR = CardColorEnum.POKER_PLAYER_GRAY;
-	private static final AbstractCard.CardRarity RARITY = CardRarity.UNCOMMON;
+	private static final AbstractCard.CardRarity RARITY = CardRarity.RARE;
 	private static final AbstractCard.CardTarget TARGET = CardTarget.SELF;
 
 	public FakeSymbols() {
